104e1d96f0bef6681f513053cdcaf57cc717dd85
willymateo <wjmateo@espol.edu.ec>
2024-08-04 14:01:44 -0500

    Update CV

---
+++ src/app/Sections/Hero.tsx
@@ -0,0 +1,42 @@
+import { Rocket } from "iconoir-react";
+import Image from "next/image";
+
+import { PrimaryButton } from "../shared/Buttons/Primary";
+import { animaticSCBold } from "@/shared/fonts";
+
+const Hero = () => (
+  <div className="min-h-screen flex flex-col items-center justify-center justify-items-center gap-10 sm:grid sm:grid-cols-[5fr_4fr] py-20">
+    <Image
+      className="rounded-full sm:order-2"
+      alt="Willy Mateo profile photo"
+      src="/images/profile.jpeg"
+      height={400}
+      width={400}
+    />
+
+    <div className="flex flex-col justify-center items-center gap-8 max-w-[600px]">
+      <div className="flex flex-col justify-center items-center gap-1">
+        <h1 className={`text-4xl sm:text-5xl text-center ${animaticSCBold.className}`}>
+          I'm Willy Mateo,
+        </h1>
+
+        <h2 className="text-xl sm:text-3xl text-center">
+          a full-stack developer dedicated to creating innovative web solutions
+        </h2>
+      </div>
+
+      <Rocket className="w-12 h-12 text-slate-300" />
+
+      <div className="flex flex-col justify-center items-center gap-5">
+        <h2 className="text-xl sm:text-3xl text-center">Let’s build something amazing together.</h2>
+
+        <div className="flex flex-row justify-center items-center gap-5 flex-wrap">
+          <PrimaryButton>Explore my work</PrimaryButton>
+          <PrimaryButton>Get in touch</PrimaryButton>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export { Hero };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { Experience } from "./Sections/Experience";
 import { TechStack } from "./Sections/TechStack";
 import { Projects } from "./Sections/Projects";
 import { AboutMe } from "./Sections/AboutMe";
-import { Welcome } from "./Sections/Welcome";
 import { Hobbies } from "./Sections/Hobbies";
 import { Footer } from "./shared/Footer";
+import { Hero } from "./Sections/Hero";
 
 const Home = () => (
   <main className="min-h-screen flex flex-col gap-20">
     <div className="flex flex-col items-strech justify-center gap-20 px-10">
-      <Welcome />
+      <Hero />
       <AboutMe />
       <TechStack />
       <Projects />
