097878a7dac410726e6883d39085aee81249dd26
willymateo <wjmateo@espol.edu.ec>
2025-01-03 15:54:50 -0500

    Add locales for Achievements section

--- src/app/[locale]/Sections/Achievements/Cards/GithubFoundationsCertification.tsx
+++ src/app/[locale]/Sections/Achievements/Cards/GithubFoundationsCertification.tsx
@@ -1,34 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
+import { useTranslations } from "next-intl";
 
 import { PrimaryButton } from "@/app/[locale]/shared/Buttons/Primary";
 import { Link } from "@/i18n/routing";
 import { Card } from "./Card";
 
-const GithubFoundationsCertification = () => (
-  <Card
-    description="This certification validates expertise in essential GitHub skills, including repositories, commits, branching, markdowns, and project management. It's designed to showcase entry-level DevOps skills and modern development practices with GitHub tools like Actions, Codespaces, GitFlow and Projects. Key Skills Validated: Git & GitHub Basics, Collaboration & Modern Development Features, Continuous Integration (CI), Continuous Delivery (CD), Secure Repository Management, GitHub Administration."
-    href="https://resources.github.com/learn/certifications/"
-    imgAlt="Github Foundations Certification credential"
-    imgUrl="/images/github-foundations-credential.png"
-    title="Github Foundations Certification"
-    startDate={new Date(2024, 11, 1)}
-    endDate={new Date(2027, 11, 1)}
-    subTitle="Certification"
-    imgHeight={200}
-    imgWidth={200}
-  >
-    <Link
-      href="https://www.credly.com/badges/90bdfedb-5212-4a93-ae60-1c5ebedfdcd3/linked_in_profile"
-      rel="noreferrer"
-      target="_blank"
+const GithubFoundationsCertification = () => {
+  const t = useTranslations("home.achievements.githubFoundationsCertification");
+
+  return (
+    <Card
+      description={`${t("This certification validates expertise in essential GitHub skills, including repositories, commits, branching, markdowns, and project management It's designed to showcase entry-level DevOps skills and modern development practices with GitHub tools like Actions, Codespaces, GitFlow and Projects Key Skills Validated: Git & GitHub Basics, Collaboration & Modern Development Features, Continuous Integration (CI), Continuous Delivery (CD), Secure Repository Management, GitHub Administration")}.`}
+      href="https://resources.github.com/learn/certifications/"
+      imgAlt="Github Foundations Certification credential"
+      imgUrl="/images/github-foundations-credential.png"
+      title="Github Foundations Certification"
+      startDate={new Date(2024, 11, 1)}
+      endDate={new Date(2027, 11, 1)}
+      subTitle={t("Certification")}
+      imgHeight={200}
+      imgWidth={200}
     >
-      <PrimaryButton>
-        <FontAwesomeIcon icon={faNewspaper} className="w-[24px] h-[24px]" />
-        <p className="text-center">View credential</p>
-      </PrimaryButton>
-    </Link>
-  </Card>
-);
+      <Link
+        href="https://www.credly.com/badges/90bdfedb-5212-4a93-ae60-1c5ebedfdcd3/linked_in_profile"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <PrimaryButton>
+          <FontAwesomeIcon icon={faNewspaper} className="w-[24px] h-[24px]" />
+          <p className="text-center">{t("View credential")}</p>
+        </PrimaryButton>
+      </Link>
+    </Card>
+  );
+};
 
 export { GithubFoundationsCertification };
