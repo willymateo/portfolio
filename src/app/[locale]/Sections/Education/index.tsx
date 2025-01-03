a226c8d4f77b7603d8386dd55a54ff1005a7e2b5
willymateo <wjmateo@espol.edu.ec>
2025-01-03 15:43:13 -0500

    Add locales for education section

--- src/app/[locale]/Sections/Education/index.tsx
+++ src/app/[locale]/Sections/Education/index.tsx
@@ -1,15 +1,21 @@
+import { useTranslations } from "next-intl";
+
 import { Container } from "../Container";
 import { SECTIONS } from "../constants";
 import { ESPOL } from "./Cards/ESPOL";
 import { CSB } from "./Cards/CSB";
 
-const Education = () => (
-  <Container id={SECTIONS.EDUCATION} title="Education">
-    <div className="flex flex-col gap-20">
-      <ESPOL />
-      <CSB />
-    </div>
-  </Container>
-);
+const Education = () => {
+  const t = useTranslations("home.education");
+
+  return (
+    <Container id={SECTIONS.EDUCATION} title={t("Education")}>
+      <div className="flex flex-col gap-20">
+        <ESPOL />
+        <CSB />
+      </div>
+    </Container>
+  );
+};
 
 export { Education };
