8b97a1c81d3a319401a738fcff6de323007f2d83
willymateo <wjmateo@espol.edu.ec>
2025-01-31 23:28:11 -0500

    Add CommunityMomentsSlider for sm

--- src/app/[locale]/Sections/TechCommunity/index.tsx
+++ src/app/[locale]/Sections/TechCommunity/index.tsx
@@ -1,9 +1,12 @@
 import { CommunityMomentsSelector } from "./CommunityMomentsSelector";
+import { CommunityMomentsSlider } from "./CommunityMomentsSlider";
 
 import "./index.css";
 
 const TechCommunity = () => (
   <section className="tech-community-container">
+    <CommunityMomentsSlider className="flex xl:hidden" />
+
     <CommunityMomentsSelector className="hidden xl:flex" />
   </section>
 );
